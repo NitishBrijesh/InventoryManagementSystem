fa2a3235ffd04f07532fdc62bb04ec532250498e
Brijesh Pathak <brijshpathak@gmail.com>
2020-03-22 16:20:21 +0100

    spring mvc first controller

---
+++ IMS/ims/src/main/java/com/ims/nb/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.ims.nb.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+	
+	
+}
--- IMS/ims/src/main/java/com/ims/nb/model/ProductBean.java
+++ IMS/ims/src/main/java/com/ims/nb/model/ProductBean.java
@@ -1,6 +1,60 @@
 package com.ims.nb.model;
 
-public class ProductBean {
+import java.util.List;
 
+public class ProductBean {
+	
+	private int ID;
+	private String name;
+	private String description;
+	private String manufacturer;
+	private List<Integer> categoryID;
+	private List<Integer> typeID;
+	private float price;
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public List<Integer> getCategoryID() {
+		return categoryID;
+	}
+	public void setCategoryID(List<Integer> categoryID) {
+		this.categoryID = categoryID;
+	}
+	public List<Integer> getTypeID() {
+		return typeID;
+	}
+	public void setTypeID(List<Integer> typeID) {
+		this.typeID = typeID;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
 
 }
